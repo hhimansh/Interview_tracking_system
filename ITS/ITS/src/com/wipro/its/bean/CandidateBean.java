e1916c6256f6af634953ed4694bb1fc490b7d0a6
hhimansh <40122705+hhimansh@users.noreply.github.com>
2021-11-13 14:01:10 -0500

    Create CandidateBean.java

---
+++ ITS/ITS/src/com/wipro/its/bean/CandidateBean.java
@@ -0,0 +1,68 @@
+package com.wipro.its.bean;
+
+public class CandidateBean {
+	String candidateID;
+	String primarySkills;
+	String secondarySkills;
+	double experience;
+	String qualification;
+	String designation;
+	int noticePeriod;
+	String location;
+	int shareDetails;
+	
+	public String getCandidateID() {
+		return candidateID;
+	}
+	public void setCandidateID(String candidateID) {
+		this.candidateID = candidateID;
+	}
+	public String getPrimarySkills() {
+		return primarySkills;
+	}
+	public void setPrimarySkills(String primarySkills) {
+		this.primarySkills = primarySkills;
+	}
+	public String getSecondarySkills() {
+		return secondarySkills;
+	}
+	public void setSecondarySkills(String secondarySkills) {
+		this.secondarySkills = secondarySkills;
+	}
+	public double getExperience() {
+		return experience;
+	}
+	public void setExperience(double experience) {
+		this.experience = experience;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public int getNoticePeriod() {
+		return noticePeriod;
+	}
+	public void setNoticePeriod(int noticePeriod) {
+		this.noticePeriod = noticePeriod;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getShareDetails() {
+		return shareDetails;
+	}
+	public void setShareDetails(int shareDetails) {
+		this.shareDetails = shareDetails;
+	}
+}
